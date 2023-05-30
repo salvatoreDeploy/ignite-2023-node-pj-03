68f274b78ab2b3546cf74cf147c2b07712832d04
salvatoreDeploy <liderhenrique@gmail.com>
2023-05-30 17:39:33 -0300

    feat: using jwt and applied as a middleware

--- src/http/controllers/profile.controller.ts
+++ src/http/controllers/profile.controller.ts
@@ -1,11 +1,17 @@
 import { FastifyReply, FastifyRequest } from 'fastify'
+import { makeGetUserProfileService } from 'src/services/factories/make-getUserProfile.sevices'
 
 export async function profileontroller(
   request: FastifyRequest,
   reply: FastifyReply,
 ) {
-  await request.jwtVerify()
-  console.log(request.user.sub)
+  const getUserProfile = makeGetUserProfileService()
 
-  return reply.status(200).send()
+  const { user } = await getUserProfile.execute({
+    userId: request.user.sub,
+  })
+
+  Reflect.deleteProperty(user, 'password_hash')
+
+  return reply.status(200).send({ user })
 }
--- src/http/routes.ts
+++ src/http/routes.ts
@@ -2,11 +2,12 @@ import { FastifyInstance } from 'fastify'
 import { registerController } from './controllers/resgiter.controller'
 import { authenticateController } from './controllers/authenticate.controller'
 import { profileontroller } from './controllers/profile.controller'
+import { verifyJWT } from './middleware/verifyJwt'
 
 export async function appRoutes(app: FastifyInstance) {
   app.post('/users', registerController)
   app.post('/session', authenticateController)
 
   /*  Authenticated */
-  app.get('/me', profileontroller)
+  app.get('/me', { onRequest: verifyJWT }, profileontroller)
 }
