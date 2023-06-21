a270cb149c243a1d4bd86092ad8a1470e9f9f1cc
salvatoreDeploy <liderhenrique@gmail.com>
2023-06-21 18:19:19 -0300

    feat: middleware verifyUserRole

--- src/http/controllers/checkins/routes.ts
+++ src/http/controllers/checkins/routes.ts
@@ -4,6 +4,7 @@ import { FetchUserMetricHistoryController } from './fetchUserMetricHistory.contr
 import { GetUserMetricController } from './getUserMetric.controller'
 import { CreateCheckInController } from './createCheckin.controller'
 import { ValidateCheckInController } from './validateCheckin.controller'
+import { verifyUserRole } from 'src/http/middleware/verifyUserRole'
 
 export async function checkInRoutes(app: FastifyInstance) {
   app.addHook('onRequest', verifyJWT)
@@ -11,5 +12,9 @@ export async function checkInRoutes(app: FastifyInstance) {
   app.get('/check-ins/history', FetchUserMetricHistoryController)
   app.get('/check-ins/metric', GetUserMetricController)
   app.post('/check-in/:gymId', CreateCheckInController)
-  app.patch('/check-in/:checkInId/validate', ValidateCheckInController)
+  app.patch(
+    '/check-in/:checkInId/validate',
+    { onRequest: [verifyUserRole('ADMIN')] },
+    ValidateCheckInController,
+  )
 }
--- src/http/controllers/gyms/routes.ts
+++ src/http/controllers/gyms/routes.ts
@@ -3,11 +3,16 @@ import { verifyJWT } from 'src/http/middleware/verifyJwt'
 import { CreateGymController } from './createGym.controller'
 import { SearchGymController } from './searchGym.Controller'
 import { FetchNearbyGYmsController } from './fetchNearbyGyms.controller'
+import { verifyUserRole } from 'src/http/middleware/verifyUserRole'
 
 export async function gymsRoutes(app: FastifyInstance) {
   app.addHook('onRequest', verifyJWT)
 
-  app.post('/creategym', CreateGymController)
+  app.post(
+    '/creategym',
+    { onRequest: [verifyUserRole('ADMIN')] },
+    CreateGymController,
+  )
   app.get('/searchgym', SearchGymController)
   app.get('/fetchgym', FetchNearbyGYmsController)
 }
